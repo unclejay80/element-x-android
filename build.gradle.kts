0ce3e6022e274d8978163118cfafe56b643ed88f
Jürgen Wischer <juergen.wischer@bwi.de>
2025-05-28 20:33:21 +0200

    ci: fix job

--- build.gradle.kts
+++ build.gradle.kts
@@ -180,10 +180,6 @@ tasks.register("runQualityChecks") {
     gradle.startParameter.isContinueOnFailure = true
 }
 
-tasks.register("generateAggregateSbom") {
-    dependsOn("cyclonedxAggregateBom")
-}
-
 // Make sure to delete old screenshots before recording new ones
 subprojects {
     val snapshotsDir = File("${project.projectDir}/src/test/snapshots")
